cc81a456ca7818623945ed5f09478aa6fbea760c
ILLUZOR <mishaliashenko33@gmail.com>
2026-03-28 12:58:39 +0300

    Update AutoClick.py

--- AutoClick.py
+++ AutoClick.py
@@ -2,16 +2,21 @@ import keyboard
 import mouse
 import time
 
-print("Нажмите <=>, чтобы запустить или остановить автокликер")
+print("Нажмите '=' для запуска/остановки")
+
+work = False
+
 def change():
     global work
     work = not work
+    state = "ЗАПУЩЕН" if work else "ОСТАНОВЛЕН"
+    print(f"Статус: {state}")
 
-
-work = False
 keyboard.add_hotkey('=', change)
 
 while True:
     if work:
         mouse.click(button='left')
-        time.sleep(0.00001)
+        time.sleep(0.1) 
+    else:
+        time.sleep(0.1)  
